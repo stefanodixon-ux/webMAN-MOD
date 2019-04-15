24b4953f23baf6ae736f347f89822604917ff12f
aldostools <aldostools@gmail.com>
2019-04-14 21:54:20 -0400

    webMAN MOD 1.47.15
    - Fixed regression accessing /dev_blind

--- main.c
+++ main.c
@@ -1649,12 +1649,14 @@ parse_request:
 			{
 				// /dev_blind          auto-enable & access /dev_blind
 				// /dev_blind?         shows status of /dev_blind
-				// /dev_blind?0        mounts /dev_blind
+				// /dev_blind?1        mounts /dev_blind
 				// /dev_blind?enable   mounts /dev_blind
-				// /dev_blind?1        unmounts /dev_blind
+				// /dev_blind?0        unmounts /dev_blind
 				// /dev_blind?disable  unmounts /dev_blind
 
-				goto retry_response;
+				is_binary = FOLDER_LISTING;
+				if(file_exists(param)) goto retry_response;
+				goto html_response;
 			}
 
  #ifdef PKG_HANDLER
