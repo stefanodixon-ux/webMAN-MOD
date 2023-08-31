75b0eac63375b57c5c13aa7508bfc7a96db09575
Aldo Vargas <aldostools@gmail.com>
2023-08-31 19:16:03 -0400

    webMAN MOD 1.47.45
    - Fix copy PS2ISO from USB device

--- include/mount/mount.h
+++ include/mount/mount.h
@@ -1152,8 +1152,6 @@ static void get_last_game(char *last_path)
 #ifdef COBRA_ONLY
 static void cache_file_to_hdd(char *source, char *target, const char *basepath, char *msg)
 {
-	do_umount(false); // umount + delete previous cached file
-
 	if(*source == '/')
 	{
 		sprintf(target, "/dev_hdd0%s", basepath);
@@ -1188,6 +1186,8 @@ static void cache_file_to_hdd(char *source, char *target, const char *basepath, 
 
 		if(file_exists(target)) strcpy(source, target);
 	}
+	
+	do_umount(false); // umount + delete previous cached file
 }
 
 static void cache_icon0_and_param_sfo(char *destpath)
